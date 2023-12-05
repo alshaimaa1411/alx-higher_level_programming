8c07dadb24763569a63a97d55d62cd8688f4116c
alshaimaa <ameenareg@gmail.com>
2023-12-04 17:01:43 -0800

    t

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for x in range(0, 99):
-    print("{}, ".format(x), end = "")
-print("{}".format(99), end = "\n")
+    print("{:d}, ".format(x), end="")
+print("{}".format(99), end="\n")
