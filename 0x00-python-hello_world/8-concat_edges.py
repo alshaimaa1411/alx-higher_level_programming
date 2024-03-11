68720182e0e5d9b763fb23e7cc3fe896904a809a
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-11 16:57:17 -0700

    t

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+         language that combines remarkable power with very clear syntax"
+substring = str[39:66] + " " + str[-22:-19] + " " + str[0:7]
+print(substring)
