ce6b8497f2775e31eb128d6659b55d72c8ac3c82
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-14 14:56:29 -0700

    t

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -2,4 +2,11 @@
 def add(a, b):
     a = abs(a)
     b = abs(b)
-    return a+b
+    if a < 0 and b < 0:
+        return -a - b
+    elif a < 0 and b > 0:
+        return -a + b
+    elif a > 0 and b < 0:
+        return a - b
+    else:
+        return a + b
