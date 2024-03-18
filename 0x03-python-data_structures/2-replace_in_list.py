460b2fd4fab34259654ff07cd5a346596ed3bb2b
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-18 15:45:28 -0700

    t

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,5 +6,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > x-1:
         return my_list
     else:
-        my_list.insert(idx, element)
+        my_list[idx] = element
         return my_list
