e7418b83feef9656bc03658f6eb8ff58ed1f8e90
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-18 18:44:43 -0700

    t

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         x = len(matrix)
-        for i in range (0, x):
+        for i in range(0, x):
             y = len(matrix[i])
             for z in range(0, y):
                 if z == y:
