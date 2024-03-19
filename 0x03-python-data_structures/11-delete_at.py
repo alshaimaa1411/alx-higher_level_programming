374ac82b95974d3c6c256251d042b964f72c8142
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-19 00:15:24 -0700

    g

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,5 +3,9 @@ def delete_at(my_list=[], idx=0):
     if idx >= len(my_list):
         return my_list
     else:
-        my_list.pop(idx)
-        return my_list
+        for i in range(0, len(my_list) - 1):
+            if i != my_list[idx]:
+                n_list =+ i
+            else:
+                continoues
+        return n_list
