4615778c4588ef658dfec18c009499d462aa8142
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-22 16:56:47 -0700

    t

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,6 @@
 def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            matrix[i][j] = matrix[i][j]**2
-    return matrix
+        matrix[i][j] = matrix[i][j]**2
+    n_matrix = matrix
+    return n_matrix
