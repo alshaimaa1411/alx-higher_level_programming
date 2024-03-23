45bed4f297f50f499c90e314d1e0a8a1d323a7de
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-22 18:15:46 -0700

    t

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambada x: x * x), i)) for i in matrix]
+     return [list(map((lambda x: x * x), i)) for i in matrix]
