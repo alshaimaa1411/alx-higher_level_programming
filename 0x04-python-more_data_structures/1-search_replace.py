d0ebee6c150c8b01a331e9ed2c7541e1292960ca
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-27 14:49:50 -0700

    t

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     n_list = []
     for i in my_list:
-        if i !=  search:
+        if i != search:
             n_list.append(i)
         else:
             n_list.append(replace)
