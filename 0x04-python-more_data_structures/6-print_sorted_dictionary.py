94c65d1f9806ac24607e50acc7930d20b9ece1cf
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-28 02:21:15 -0700

    t

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    s_key = sorted(a_dictionary.keys())
+    b_dic = {key: a_dictionary[key] for key in s_key}
+    for key, value in b_dic.items():
+        print("{} : {}".format(key, value))
