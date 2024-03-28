85df2eda7e31c540b693a93d0f664d762fc7ada4
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-03-28 15:00:24 -0700

    u

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary or len(a_dictionary):
+        n_li = []
+        for key in a_dictionary.keys():
+            value = a_dictionary[key]
+            n_li.append(value)
+        n_li = sorted(n_li)
+        x = n_li[-1]
+        for key in a_dictionary.keys():
+            if x == a_dictionary[key]:
+                print(key)
+    else:
+        return None
