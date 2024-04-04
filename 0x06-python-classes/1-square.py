4daf3310f3ea40321d7dd3e2f58bca13adad0abb
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-04 16:49:21 -0700

    k

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' square class'''
+
+
+class Square:
+    ''' Square rep
+    __size (int): square size
+    '''
+    def __int__(self, size):
+        ''' int square
+        size (int): size of square
+        return: None
+        '''
+        self.__size = size
