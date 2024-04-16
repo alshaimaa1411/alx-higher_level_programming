1c31ed501b24efa9a6e55991e8611dc1ef5770b9
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-16 16:49:12 -0700

    t

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Square:
+    ''' class square'''
+    def __int__(self, size=0):
+        ''' size attri '''
+        self.size = size
+
+    def area(self):
+        '''area fun'''
+        return self.__size ** 2
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
