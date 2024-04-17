8acb7af8f78b6de7db7041a3f380dcdbe69a42ff
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-17 05:42:24 -0700

    u

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -37,7 +37,6 @@ class Rectangle:
         else:
             if value < 0:
                 raise ValueError("height must be >= 0")
-            else:
                 self.__height = value
 
     def width(self):
@@ -58,5 +57,4 @@ class Rectangle:
         else:
             if value < 0:
                 raise ValueError("width must be >= 0")
-            else:
                 self.__width = value
