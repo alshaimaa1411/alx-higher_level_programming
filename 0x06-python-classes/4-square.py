7a8b0c678851aafd7237f0ae0a4e23dea569c24d
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-18 11:41:43 -0700

    f

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,10 +8,6 @@ class Square:
         """ size attri """
         self.size = size
 
-    def area(self):
-        """area fun"""
-        return self.__size ** 2
-
     @property
     def size(self):
         """ size attru"""
@@ -22,8 +18,10 @@ class Square:
         """value fun req """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        else:
-            if value < 0:
+        if value < 0:
                 raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        self.__size = value
+
+    def area(self):
+        """ return: size"""
+        return self.__size * self.__size
