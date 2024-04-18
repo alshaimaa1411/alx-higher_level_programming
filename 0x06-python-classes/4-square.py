c439a2754892f56054986c7e8816505eca2ad59a
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-18 12:04:38 -0700

    t

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,25 +3,25 @@
 
 
 class Square:
-    """ class square"""
-    def __int__(self, size=0):
-        """ size attri """
+    """ square class"""
+    def __init__(self, size=0):
+        """ attri size"""        
         self.size = size
+
+    def area(self):
+        """ return area"""        
+        return self.__size ** 2
 
     @property
     def size(self):
-        """ size attru"""
+        """ return size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """value fun req """
+        """size req """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
-                raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """ return: size"""
-        return self.__size * self.__size
