10e434ba2284dc9901d492282222d57cc58c186a
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-23 13:53:16 -0700

    T

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """ instance class """
+    def is_same_class(obj, a_class):
+        """ eturnobj"""
+        return (type(obj) == a_class)
