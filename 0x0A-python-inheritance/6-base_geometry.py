ec2b72359a58b4c7fdc377e6a75c03fbf585ea8d
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-25 05:19:02 -0700

    g

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" empty class"""
+
+
+class BaseGeometry:
+    """ e,pty class"""
+    def area(self):
+        """ exception aea"""
+        raise Exception("area() is not implemented")
