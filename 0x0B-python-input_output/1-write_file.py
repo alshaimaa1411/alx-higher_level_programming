4911d3b4cf3a228d8c2c7c8184a44130353d59cd
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-28 16:26:31 -0700

    l

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,8 +6,4 @@ def write_file(filename="", text=""):
     """ write line"""
     with open(filename, "w", encoding="UTF8") as mfile:
         cont = mfile.write(text)
-        chcount = 0
-        for i in text:
-            for x in i:
-                chcount +=1
-        print(chcount)
+        return len(text)
