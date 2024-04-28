51fa14925ac1be3757b94c355eee9bcdfd98891c
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-28 15:04:14 -0700

    k

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" raed file"""
+
+
 def read_file(filename=""):
+    """ read file"""
     with open(filename, "r", encoding="UTF8") as file:
         cont = file.read()
         print(cont)
