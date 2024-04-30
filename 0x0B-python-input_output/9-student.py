8bfdf9be741da7df6160b51d786dcad12ec2d7d8
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-30 15:23:38 +0300

    aa

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,10 +5,12 @@
 class Student:
     """ student class"""
     def __init__(self, first_name, last_name, age):
+        """ atrri init"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ to json"""
         return self.__dict__
     
