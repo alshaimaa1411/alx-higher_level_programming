cf98bad3d407cfe61d1bca9ecd39671623c5cef6
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-30 15:06:57 +0300

    aa

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-import json
+""" json list"""
+
 from sys import argv
-""" add list"""
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+filename = "add_item.json"
+try:
+    jlist = load_from_json_file(filename)
+except FileNotFoundError:
+    jlist = []
 
-def save_to_json_file(my_obj, filename):
-    """ write in file"""
-    with open(filename, "w") as mfile:
-        j_obj = json.dumps(my_obj)
-        mfile.write(j_obj)
-        
-def load_from_json_file(filename):
-    """crate obj"""
-    with open(filename, 'r', encoding='utf-8') as mfile:
-        return json.load(mfile)
-    
-def main():
-    """ add list"""
-    filename = "add_item.json"
-    try:
-        jlist = load_from_json_file(filename)
-    except FileNotFoundError:
-        jlist = []
+for arg in argv[1:]:
+    jlist.append(arg)
 
-    for arg in argv[1:]:
-        jlist.append(arg)
-
-    save_to_json_file(jlist, filename)
+save_to_json_file(jlist, filename)
