7730c12ebe0da2e540e1ebf2e1ebb6aad4ecff8e
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-30 14:37:58 +0300

    kkk

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+""" creat obj"""
+
+
+def load_from_json_file(filename):
+    """ creat obj"""
+    with open(filename, "r") as mfile:
+        lfile = json.loads(mfile)
+        return lfile
+    
