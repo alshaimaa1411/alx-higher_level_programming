040f8b90d65e20ad02df924d0fac792ae90fbdb9
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-30 14:29:38 +0300

    kk

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,7 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """ write in file"""
     with open(filename, "w") as mfile:
         j_obj = json.dumps(my_obj)
         mfile.write(j_obj)
-        
