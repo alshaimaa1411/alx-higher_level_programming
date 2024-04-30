6622886dff757edcdd6e48d41c5afbea72aa731f
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-30 15:08:47 +0300

    ss

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,13 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
+
 try:
-    jlist = load_from_json_file(filename)
+    json_list = load_from_json_file(filename)
 except FileNotFoundError:
-    jlist = []
+    json_list = []
 
 for arg in argv[1:]:
-    jlist.append(arg)
+    json_list.append(arg)
 
-save_to_json_file(jlist, filename)
+save_to_json_file(json_list, filename)
