07ef879a06e4b38fd102a8c93295ebf587b5a459
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-04-30 16:41:26 +0300

    aa

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" student class"""
+
+
+class Student:
+    """ student class"""
+    
+    def __init__(self, first_name, last_name, age):
+        """ atrri init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ to json"""
+        return self.__dict__
+    
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
