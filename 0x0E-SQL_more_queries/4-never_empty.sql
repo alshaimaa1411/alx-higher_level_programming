1759e4775efb78be1ff0851457c769fb36cfd976
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-22 23:27:35 +0300

    4T

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,2 @@
 -- NEW TABLE
-CREATE TABLE IF NOT EXISTS id_not_null(id INT, name VARCHAR(256));
-SELECT IFNULL(id, 1) FROM id_not_null;
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
