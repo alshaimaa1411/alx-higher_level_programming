dd06788acd9e93a7b9c3e8744b50e1c1a28f3eae
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-23 01:36:48 +0300

    14t

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 --Write a script that lists all genres 
-SELECT tv_genres.name AS name
+SELECT tv_genres.name
     FROM tv_genres
         INNER JOIN tv_show_genres
         ON tv_genres.id = tv_show_genres.genre_id
