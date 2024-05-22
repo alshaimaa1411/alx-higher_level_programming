b5c9a8f4f71c6588660f29f2cd1ea48d8f1d6ee4
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-22 23:52:34 +0300

    7T

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     PRIMARY KEY(id),
     id INT NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY(state_id) REFERENCES TO id,
+    state_id INT NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES TO hbtn_0d_usa.states(id),
     name  VARCHAR(256) NOT NULL
 );
