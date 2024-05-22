1c1d72dff30d2679c0d07091f0c8d69ee1bd9797
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-23 02:10:47 +0300

    14T

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 --Write a script that lists all genres 
-SELECT g.`name`
-    FROM `tv_genres` AS g
-        INNER JOIN `tv_show_genres` AS s
-        ON g.`id` = s.`genre_id`
-
-        INNER JOIN `tv_shows` AS t
-        ON t.`id` = s.`show_id`
-        WHERE t.`title` = "Dexter"
-ORDER BY g.name;
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN tv_show_genres 
+ON tv_genres.id = tv_show_genres.show_id
+LEFT JOIN tv_shows 
+ON tv_show_genres.genre_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY tv_genres.name
+ORDER BY tv_genres.name ASC;
