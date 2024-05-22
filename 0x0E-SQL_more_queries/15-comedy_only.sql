80d75a3329d8eda6c1d0c8048457904fa865acc3
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-23 02:03:05 +0300

    15T

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,9 @@
 -- show list
 SELECT tv_shows.title
 FROM tv_shows AS t
-LEFT JOIN tv_show_genres AS ts
+RIGHT JOIN tv_show_genres AS ts
 ON t.id = ts.show_id
-LEFT JOIN tv_genres AS g
+RIGHT JOIN tv_genres AS g
 ON g.id = ts.genre_id
 WHERE g.name = "Comedy "
-GROUP BY t.title
 ORDER BY t.title;
