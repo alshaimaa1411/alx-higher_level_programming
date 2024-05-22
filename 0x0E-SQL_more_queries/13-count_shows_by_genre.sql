edae3858e1173c183c181d18364be6a8a9078c68
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-23 00:58:42 +0300

    13T

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 --Write a script that lists all genres 
 SELECT g.name AS genre,
-COUNT (*) AS number_of_shows
+    COUNT(*) AS number_of_shows
 FROM tv_genres AS g
     INNER JOIN tv_show_genres AS t
        ON g.id = t.genre_id
