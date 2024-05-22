59994aa4d7ee31741124aba2d22fae3e91696238
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-23 01:55:16 +0300

    15t

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,8 +2,8 @@
 SELECT tv_shows.title
 FROM tv_shows AS t
 INNER JOIN tv_show_genres AS ts
-ON t.id = ts.soe_id
+ON t.id = ts.show_id
 INNER JOIN tv_genres AS g
-ON d.id = ts.genre_id
+ON g.id = ts.genre_id
 WHERE g.name = "Comedy "
 ORDER BY tv_shows.title
