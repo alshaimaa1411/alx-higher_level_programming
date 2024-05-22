b86af9e9b5963c9e2d39d4031d4fb2ae98fad2d6
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-05-23 02:23:04 +0300

    16T

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,6 @@
 -- SHOW LIST
-SELECT tv_shows.title
+SELECT title, name
 FROM tv_shows
-LEFT JOIN tv_show_genres 
-ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres 
-ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
