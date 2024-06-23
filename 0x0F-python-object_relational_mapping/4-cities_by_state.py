6632e6a3ff6f011d418021c7e56e2b6ac68a8a87
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-06-24 00:38:53 +0300

    1t

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = mydb.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name
-                FROM cities INNER JOIN states ON cities.state_id = states.id""")
+                FROM cities INNER JOIN 
+                states ON cities.state_id = states.id""")
     city = cur.fetchall()
     for x in city:
         print(x)
