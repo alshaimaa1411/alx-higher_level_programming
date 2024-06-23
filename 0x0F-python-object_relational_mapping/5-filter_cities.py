e19aa80350ab05e1df880611d7d710fcf8c0f80f
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-06-24 00:56:20 +0300

    1t

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,8 +5,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    mydb = MySQLdb.connect(host="localhost", usernsme=sys.argv[1], password=sys.argv[2],
-                           dbname=sys.argv[3], port=3306)
+    mydb = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = mydb.cursor()
     state = sys.argv[4]
     cur.execute("""SELECT cities.name FROM cities
