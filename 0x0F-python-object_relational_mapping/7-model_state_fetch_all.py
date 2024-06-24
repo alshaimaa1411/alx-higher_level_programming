ab8f5c64f55def8d15c324149d953e2d056c81f6
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-06-24 16:39:48 +0300

    t

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,8 +4,8 @@ state obj
 """
 import sys
 from model_state import Base, State
-from SQLAlchemy import (create_engine)
-from SQLAlchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
