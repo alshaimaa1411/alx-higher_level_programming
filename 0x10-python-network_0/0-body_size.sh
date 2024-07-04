71333948b6a81e22ecd781537359215e13d93e46
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-07-05 02:33:55 +0300

    1r

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
+# Sends a request to a URL, retrieves the size of the response body in bytes
 
-# Check if URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-# Send a GET request to the URL and capture the response body size
-response=$(curl -sI "$url" | grep -i 'content-length' | awk '{print $2}')
-
-# Check if response contains Content-Length header
-if [ -z "$response" ]; then
-    echo "Error: Content-Length header not found in the response."
-    exit 1
-fi
-
-echo "$response"
+curl -sI "$1" | grep -i 'content-length' | awk '{print $2}'
