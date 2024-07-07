1aaada41eb3178a83b92174ac38fff4094bccf7b
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-07-07 07:39:03 -0700

    m

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const x = process.argv.length;
-let a = process.argv[2];
-if (x === 2) {
-	  console.log('No argument');
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-	console.log(a);
+  console.log(process.argv[2]);
 }
