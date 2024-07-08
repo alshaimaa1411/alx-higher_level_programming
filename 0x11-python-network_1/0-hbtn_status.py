acd45299205d2385a2ad0d5a549b26a765bcb170
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-07-08 06:23:57 -0700

    F

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,3 +4,7 @@ from urllib.request import urlopen
 url = "https://alx-intranet.hbtn.io/status"
 with urlopen(url) as response:
     body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
