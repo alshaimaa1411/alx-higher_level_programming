ba115e2126a4c90701488dd49a5db13ae6122be4
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-07-08 19:45:11 -0700

    f

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 def fetch_status():
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
-    
+
     print("Body response:")
     print(f"\t- type: {type(response.text)}")
     print(f"\t- content: {response.text}")
