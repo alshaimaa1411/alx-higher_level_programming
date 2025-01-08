cc43126e6f432cbea8522b0c266fe8f392ef769d
Momen Ahmed <momen.ahmed771647@gmail.com>
2025-01-08 17:00:42 +0200

    s

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,13 +4,11 @@ const args = process.argv.slice(2);
 
 if (args[0] == null) {
   console.log('No argument');
-}
-else {
+} else {
   const x = parseInt(args[0]);
   if (Number.isInteger(x)) {
     console.log('My number: ' + x);
-  }
-  else {
+  } else {
     console.log('Not a numbe');
   }
 }
