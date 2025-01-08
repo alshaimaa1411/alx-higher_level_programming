71b04e2b24623162d75fc962c55258bd327b171b
Momen Ahmed <momen.ahmed771647@gmail.com>
2025-01-08 17:24:17 +0200

    s

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 
 if (Number.isInteger(x)) {
-  for (let i = 1; i<= x; i++){
+  for (let i = 1; i <= x; i++) {
     console.log('C is fun');
   }
 } else {
