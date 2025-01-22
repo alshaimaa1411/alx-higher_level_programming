2d60b5ed146a9e0bd6134378000df8ec1001add0
Momen Ahmed <momen.ahmed771647@gmail.com>
2025-01-22 17:24:38 +0200

    s

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,10 +4,10 @@ const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 
 function factorial (a) {
-    if (a <= 1) {
-      return 1;
-    } 
-    return a * factorial(a-1)
+  if (a <= 1) {
+    return 1;
+  } 
+  return a * factorial(a-1)
 }
 
 console.log(factorial(Number(x)));
