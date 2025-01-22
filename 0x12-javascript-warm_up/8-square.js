8804f76138bcc8dfd51f25ced647c932c17bfc7e
Momen Ahmed <momen.ahmed771647@gmail.com>
2025-01-22 13:15:27 +0200

    s

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const x = parseInt[0];
+
+if (Number.isInteger(x)) {
+  for (let i = 1; i <= x; i++) {
+    for (let a = 1; a <=x; a++) {
+      console.log('X');
+    }
+  }
+} else {
+  console.log('Missing size');
+}
