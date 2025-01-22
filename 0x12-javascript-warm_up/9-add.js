154d97d50dd871d6d05ab48e41a3d5d186f30b15
alshaimaa1411 <ahmedameen1411@gmail.com>
2025-01-22 04:24:22 -0800

    g

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
+const args = process.argv.slice(2);
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
+
 function add(a, b){
-  return console.log(a+b);
+  console.log(a+b);
 }
