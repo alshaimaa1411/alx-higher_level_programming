7365d3d73d49be6e8162081f4d7ab23f39260162
Momen Ahmed <momen.ahmed771647@gmail.com>
2025-01-22 14:34:05 +0200

    s

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-const a = parseInt(args[0]);
-const b = parseInt(args[1]);
-
 function add(a, b){
   console.log(a+b);
 }
+
+
+const args = process.argv.slice(2);
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
