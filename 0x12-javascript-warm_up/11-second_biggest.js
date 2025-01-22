24846b99ef4d6dab3d59265e00e06fbfe343b19e
Momen Ahmed <momen.ahmed771647@gmail.com>
2025-01-22 16:16:55 +0200

    sw

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const arr = process.argv.silice(2).map(Number);
+  const arr = process.argv.slice(2).map(Number);
   arr.sort((a, b) => b - a);
   console.log(arr[1]);
 }
