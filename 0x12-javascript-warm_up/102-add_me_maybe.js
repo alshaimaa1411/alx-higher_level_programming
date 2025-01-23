c4dd04f691cf634ceb94243abbce47378a1d4ec7
Momen Ahmed <momen.ahmed771647@gmail.com>
2025-01-23 16:14:07 +0200

    s

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-const x = parseInt(args[2]);
-
 exports.addMeMaybe = function (number, theFunction) {
-   return theFunction(number + x);
+   return theFunction(number++);
 };
