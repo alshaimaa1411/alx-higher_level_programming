999a6a9bfa41d45150e38ccc3852b4de6bcc2c1e
Momen Ahmed <momen.ahmed771647@gmail.com>
2025-02-18 17:22:33 +0200

    s

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+        for (let a = 0; a < this.width; a++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+  rotate () {
+    let x = this.height;
+    this.height = this.width;
+    this.width = x;
+  }
+  double () {
+    this.height = this.height * 2;
+    this.width  = this.width * 2;
+  }
+}
+module.exports = Rectangle;
+ 
